77df8dda1240037992e93783d44716d487d3594b
Roosham <markhamonca@codeninjas.com>
2023-11-26 13:51:14 -0500

    close player test

--- Space Invaders (AL)/Assets/Enemy.cs
+++ Space Invaders (AL)/Assets/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,12 +13,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(bulletPrefab)
-        }
-
         
     }
 }
