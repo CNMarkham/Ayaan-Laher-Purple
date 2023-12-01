26d8f992f7bfa00c4cf63ea1c5d5b476039579e0
Chelsea <markhamonca@codeninjas.com>
2023-12-01 17:51:01 -0500

    clone

--- Space Invaders (AL)/Assets/Player.cs
+++ Space Invaders (AL)/Assets/Player.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject bulletPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(bulletPrefab)
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
         }
-
-        
     }
+
 }
