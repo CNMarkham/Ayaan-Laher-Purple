25d4ffc462febe206f3d09922a7fb008c4cece79
Roosham <markhamonca@codeninjas.com>
2023-12-10 13:56:10 -0500

    done

--- Space Invaders (AL)/Assets/Enemy.cs
+++ Space Invaders (AL)/Assets/Enemy.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private Animator animator;
+    public float speed;
+    static private Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        direction = Vector2.right;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        animator.SetTrigger("death");
+        animator.SetTrigger("kill");
         Destroy(gameObject, 1f);
         Destroy(collision.gameObject);
     }
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(direction * Time.deltaTime * speed);
+
+        if (transform.position.x > 8f)
+        {
+            direction = Vector2.left;
+            MoveDown();
+        }
+
+
+        if (transform.position.x < -8f)
+        {
+            direction = Vector2.right;
+            MoveDown();
+        }
+    }
+
+
+    private void MoveDown()
+    {
+        foreach (Enemy enemy in FindObjectsOfType(typeof(Enemy)))
+        {
+            enemy.transform.Translate(Vector2.down);
+        }
+
     }
 }
+
--- Space Invaders (AL)/Assets/bullet.cs
+++ Space Invaders (AL)/Assets/bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {   
-    public float s;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
