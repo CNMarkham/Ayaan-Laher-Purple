096051ed84685528d04809026db9de02f6c24fc7
Ryan <markhamonca@codeninjas.com>
2024-01-14 12:50:52 -0500

    40

--- Asteroids (AL)/Assets/Bscript.cs
+++ Asteroids (AL)/Assets/Bscript.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class Bscript : MonoBehaviour
 {
-
     public float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.Translate(transform.right * Time.deltaTime * speed, Space.World);
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(transform.right * Time.deltaTime * speed, Space.World);
     }
 }
--- Asteroids (AL)/Assets/ScreenWarp.cs
+++ Asteroids (AL)/Assets/ScreenWarp.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ScreenWarp : MonoBehaviour
 {
+    private Vector2 screenMin;
+    private Vector2 screenMax;
     // Start is called before the first frame update
     void Start()
     {
-        
+        screenMin = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        screenMax = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = transform.position.x;
+        float y = transform.position.y;
     }
 }
