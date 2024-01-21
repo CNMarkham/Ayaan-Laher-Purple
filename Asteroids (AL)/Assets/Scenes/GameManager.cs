60c5a37016116150c2f64bc739e620b808ca453b
Kelton <markhamonca@codeninjas.com>
2024-01-21 12:59:35 -0500

    im done

--- Asteroids (AL)/Assets/Scenes/GameManager.cs
+++ Asteroids (AL)/Assets/Scenes/GameManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameManager : MonoBehaviour
 {
     public GameObject rockPrefab;
+    public Transform[] spawnPoints;
+
+    private void SpawnRocks()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 randomPosition = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+            Instantiate(rockPrefab, randomPosition, Quaternion.identity);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 randomPosition = new 
-        Instanstiate(rockPrefab, Vector3.zero, Quaternion.identity);
+        InvokeRepeating("SpawnRocks", 0f, 10f);
     }
 }
-S
