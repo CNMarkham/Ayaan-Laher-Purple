397ca9314939dec80a3c0e850580a6b44761d96e
Alec <markhamonca@codeninjas.com>
2024-02-25 12:50:39 -0500

    o
    ok

--- Pac-Man(AL)/Assets/Scripts/Movement.cs
+++ Pac-Man(AL)/Assets/Scripts/Movement.cs
@@ -22,7 +22,10 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      if (nextDirection != Vector2.zero)
+        {
+            SetDirection(nextDirection);
+        }
     }
 
     private void FixedUpdate()
@@ -31,5 +34,24 @@ public class Movement : MonoBehaviour
         Vector2 translation = direction * speed * Time.fixedDeltaTime;
 
         rb.MovePosition(position + translation);
+    }
+
+    private bool Occupied(Vector2 newDirection)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, newDirection, 1.5f, obstacleLayer);
+        return hit.collider != null;
+    }
+
+    protected void SetDirection(Vector2 newDirection)
+    {
+        if (!Occupied(newDirection))
+        {
+            direction = newDirection;
+            nextDirection = Vector2.zero;
+        }
+        else
+        {
+            nextDirection - newDirection;
+        }
     }
 }
--- Pac-Man(AL)/Assets/Scripts/PacMan.cs
+++ Pac-Man(AL)/Assets/Scripts/PacMan.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class PacMan : Movement
 {
-    private bool Occupied(Vector2 newDirection)
-    {
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, newDirection, 1.5f, obstacleLayer);
-        return hit.collider != null;
-    }
+  
 }
 
