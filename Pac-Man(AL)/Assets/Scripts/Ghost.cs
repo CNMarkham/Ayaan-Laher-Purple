a7d2db5f0232996bef030060f6711875af6d5a78
Madelyn <markhamonca@codeninjas.com>
2024-03-17 12:45:55 -0400

    ighost script

--- Pac-Man(AL)/Assets/Scripts/Ghost.cs
+++ Pac-Man(AL)/Assets/Scripts/Ghost.cs
@@ -4,5 +4,55 @@ using UnityEngine;
 
 public class Ghost : Movement
 {
+    public GameObject body;
+    public GameObject eyes;
+    public GameObject blue;
+    public GameObject white;
+    public bool atHome;
+    public float homeDirection;
+    private bool frightened;
+    private void Awake()
+    {
+      
+    }
+    protected override void ChildUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+
+        if (node != null)
+        {
+
+        }
+    }
+
+    private void LeaveHome()
+    {
+
+    }
+
+    public void Frighten()
+    {
+
+    }
+
+    private void Flash()
+    {
+
+    }
+
+    private void Reset()
+    {
+         
+    }
 }
+
