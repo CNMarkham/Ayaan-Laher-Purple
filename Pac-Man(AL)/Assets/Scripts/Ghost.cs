3555d203025533e05ccfbea92d000745cb870af1
Emma <markhamonca@codeninjas.com>
2024-03-22 17:58:57 -0400

    ghost 3 part

--- Pac-Man(AL)/Assets/Scripts/Ghost.cs
+++ Pac-Man(AL)/Assets/Scripts/Ghost.cs
@@ -9,11 +9,16 @@ public class Ghost : Movement
     public GameObject blue;
     public GameObject white;
     public bool atHome;
-    public float homeDirection;
+    public float homeDuration;
     private bool frightened;
     private void Awake()
     {
-      
+        body.SetActive(true);
+        eyes.SetActive(true);
+        blue.SetActive(false);
+        white.SetActive(false);
+        frightened = false;
+        Invoke("LeaveHome", homeDuration);
     }
     protected override void ChildUpdate()
     {
@@ -22,7 +27,10 @@ public class Ghost : Movement
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (atHome && collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            SetDirection(-direction);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,13 +39,28 @@ public class Ghost : Movement
 
         if (node != null)
         {
+            int index = Random.Range(0, node.avaliableDirections.Count);
 
+            index += 1;
+
+            if(index == node.avaliableDirections.Count)
+            {
+                index = 0;
+            }
+
+            SetDirection(node.avaliableDirections[index]);
         }
     }
 
     private void LeaveHome()
     {
-
+        transform.position = new Vector3(0, 2.5f, -1f);
+        direction = new Vector2(-1, 0);
+        atHome = false;
+        body.SetActive(true);
+        eyes.SetActive(true);
+        white.SetActive(true);
+        blue.SetActive(true);
     }
 
     public void Frighten()
