8322a04c625eafde1781a1e00cca1edd3bf12215
Bernice <markhamonca@codeninjas.com>
2024-05-03 17:59:50 -0400

    buhg

--- Mario(AL)/Mario(AL)/Assets/Scenes/Scripts/PlayerMovement.cs
+++ Mario(AL)/Mario(AL)/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -9,11 +9,13 @@ public class PlayerMovement : MonoBehaviour
 
     private RaycastHit2D hit;
     public float jumpForce;
+    private bool jumping;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumping = false;
     }
 
     // Update is called once per frame
@@ -29,12 +31,29 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         hit = Physics2D.CircleCast(rb.position, 0.25f, Vector2.down, 0.375f, LayerMask.GetMask("Default"));
+        Debug.Log(hit);
 
         if (hit.collider != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("hello");
+            Vector3 velocity = rb.velocity;
+            velocity.y = jumpForce;
+            rb.velocity = velocity;
+            jumping = true;
+            Invoke("ResetJumping", 0.5f);
+        }
+
+
+        if (jumping && Input.GetKey(KeyCode.Space))
         {
             Vector3 velocity = rb.velocity;
             velocity.y = jumpForce;
             rb.velocity = velocity;
         }
+    }
+
+    private void ResetJumping()
+    {
+      jumping = false;
     }
 }
