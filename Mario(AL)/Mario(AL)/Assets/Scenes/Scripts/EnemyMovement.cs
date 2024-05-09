4ee1a00955dcf2f9ad60f625615f64a883c6dea2
Bernice <markhamonca@codeninjas.com>
2024-05-09 18:56:04 -0400

    after enenmy mive

--- Mario(AL)/Mario(AL)/Assets/Scenes/Scripts/EnemyMovement.cs
+++ Mario(AL)/Mario(AL)/Assets/Scenes/Scripts/EnemyMovement.cs
@@ -6,15 +6,21 @@ public class EnemyMovement : MonoBehaviour
 {
     public float speed = 2f;
     public Vector2 direction = Vector2.left;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnBecameVisible()
+    {
+        rb.velocity = direction * speed;
     }
 }
